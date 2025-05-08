6b8c4ca613fc33e49a404a4a54647a24b6624e8f
SokolMark <sarkkofag@gmail.com>
2025-05-08 17:05:17 +0300

    fixed the code of the first query about sets

--- src/main/java/com/sokol/webgame/webgame/controller/SetController.java
+++ src/main/java/com/sokol/webgame/webgame/controller/SetController.java
@@ -2,23 +2,22 @@ package com.sokol.webgame.webgame.controller;
 
 import com.sokol.webgame.webgame.entity.Set;
 import com.sokol.webgame.webgame.service.SetService;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+@AllArgsConstructor
+@Builder
 @RestController
 public class SetController {
+    private final SetService setService;
 
-    SetService setService;
-
-    public SetController(SetService setService) {
-        this.setService = setService;
-    }
-
-    @GetMapping("set")
-    public List<Set> findAll(){
+    @GetMapping("game/sets")
+    public List<Set> findAll() {
         return setService.findAll();
     }
-
 }
--- src/main/java/com/sokol/webgame/webgame/service/SetService.java
+++ src/main/java/com/sokol/webgame/webgame/service/SetService.java
@@ -2,21 +2,19 @@ package com.sokol.webgame.webgame.service;
 
 import com.sokol.webgame.webgame.Repository.SetRepo;
 import com.sokol.webgame.webgame.entity.Set;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@AllArgsConstructor
+@Builder
 @Service
 public class SetService {
-
-    SetRepo setRepo;
-
-    public SetService(SetRepo setRepo) {
-        this.setRepo = setRepo;
-    }
+    private final SetRepo setRepo;
 
     public List<Set> findAll() {
         return setRepo.findAll();
     }
-
 }
