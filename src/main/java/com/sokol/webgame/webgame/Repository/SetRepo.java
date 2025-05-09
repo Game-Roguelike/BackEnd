9da5a03e255cc3a4a0bc6971a4587bf44eaed9b6
Mark <159955291+SokolMark@users.noreply.github.com>
2025-05-09 14:04:41 +0300

    first request for sets
    * first request for sets
    
    * fixed the code of the first query about sets
    
    * removed builder annotation

---
+++ src/main/java/com/sokol/webgame/webgame/Repository/SetRepo.java
@@ -0,0 +1,7 @@
+package com.sokol.webgame.webgame.Repository;
+
+import com.sokol.webgame.webgame.entity.Set;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SetRepo extends JpaRepository<Set, Integer> {
+}
---
+++ src/main/java/com/sokol/webgame/webgame/service/SetService.java
@@ -0,0 +1,18 @@
+package com.sokol.webgame.webgame.service;
+
+import com.sokol.webgame.webgame.Repository.SetRepo;
+import com.sokol.webgame.webgame.entity.Set;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class SetService {
+    private final SetRepo setRepo;
+
+    public List<Set> findAll() {
+        return setRepo.findAll();
+    }
+}
