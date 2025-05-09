6554655a1acb654425f2bcc6598af4d4f062020d
SokolMark <sarkkofag@gmail.com>
2025-05-09 10:00:30 +0300

    removed builder annotation

--- src/main/java/com/sokol/webgame/webgame/controller/SetController.java
+++ src/main/java/com/sokol/webgame/webgame/controller/SetController.java
@@ -3,15 +3,12 @@ package com.sokol.webgame.webgame.controller;
 import com.sokol.webgame.webgame.entity.Set;
 import com.sokol.webgame.webgame.service.SetService;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
 @AllArgsConstructor
-@Builder
 @RestController
 public class SetController {
     private final SetService setService;
--- src/main/java/com/sokol/webgame/webgame/service/SetService.java
+++ src/main/java/com/sokol/webgame/webgame/service/SetService.java
@@ -3,13 +3,11 @@ package com.sokol.webgame.webgame.service;
 import com.sokol.webgame.webgame.Repository.SetRepo;
 import com.sokol.webgame.webgame.entity.Set;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @AllArgsConstructor
-@Builder
 @Service
 public class SetService {
     private final SetRepo setRepo;
