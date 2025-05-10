f8fb7693da41c8f2f39021d52803eac6e6548d4c
Mark <159955291+SokolMark@users.noreply.github.com>
2025-05-10 18:49:16 +0300

    implemented methods for image rotation
    * implemented methods for image rotation
    
    * implemented methods for image rotation
    
    * fixed code for returning images
    
    * impoved code for returning images

---
+++ src/main/java/com/sokol/webgame/webgame/controller/GameController.java
@@ -0,0 +1,26 @@
+package com.sokol.webgame.webgame.controller;
+
+import com.sokol.webgame.webgame.entity.Set;
+import com.sokol.webgame.webgame.service.GameService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/game")
+public class GameController {
+    private final GameService gameService;
+
+    @GetMapping("/sets")
+    public List<Set> findAllSets() {
+        return gameService.findAllSets();
+    }
+
+    @GetMapping(value = "/image", produces = {MediaType.IMAGE_PNG_VALUE})
+    public @ResponseBody byte[] getImages(@RequestParam String path) {
+        return gameService.getImage(path);
+    }
+}
--- src/main/java/com/sokol/webgame/webgame/service/GameService.java
+++ src/main/java/com/sokol/webgame/webgame/service/GameService.java
@@ -3,16 +3,26 @@ package com.sokol.webgame.webgame.service;
 import com.sokol.webgame.webgame.Repository.SetRepo;
 import com.sokol.webgame.webgame.entity.Set;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @AllArgsConstructor
 @Service
-public class SetService {
+
+public class GameService {
     private final SetRepo setRepo;
 
-    public List<Set> findAll() {
+    public List<Set> findAllSets() {
         return setRepo.findAll();
+    }
+
+    @SneakyThrows
+    public byte[] getImage(String path) {
+        return new ClassPathResource(path)
+                .getInputStream()
+                .readAllBytes();
     }
 }
