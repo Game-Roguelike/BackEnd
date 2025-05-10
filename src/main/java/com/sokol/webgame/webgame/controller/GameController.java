af2354f30add6ad662c1de8bd24da68d8f6c655b
SokolMark <sarkkofag@gmail.com>
2025-05-10 17:44:00 +0300

    implemented methods for image rotation

--- src/main/java/com/sokol/webgame/webgame/controller/GameController.java
+++ src/main/java/com/sokol/webgame/webgame/controller/GameController.java
@@ -1,20 +1,27 @@
 package com.sokol.webgame.webgame.controller;
 
 import com.sokol.webgame.webgame.entity.Set;
-import com.sokol.webgame.webgame.service.SetService;
+import com.sokol.webgame.webgame.service.GameService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @AllArgsConstructor
 @RestController
-public class SetController {
-    private final SetService setService;
+@RequestMapping("game")
+public class GameController {
+    private final GameService gameService;
 
-    @GetMapping("game/sets")
-    public List<Set> findAll() {
-        return setService.findAll();
+    @GetMapping("sets")
+    public List<Set> findAllSets() {
+        return gameService.findAllSets();
+    }
+
+    @GetMapping(value = "img", produces = {MediaType.IMAGE_PNG_VALUE})
+    public @ResponseBody byte[] getImages(@RequestParam String path) throws IOException {
+        return gameService.getImages(path);
     }
 }
--- src/main/java/com/sokol/webgame/webgame/service/GameService.java
+++ src/main/java/com/sokol/webgame/webgame/service/GameService.java
@@ -3,16 +3,24 @@ package com.sokol.webgame.webgame.service;
 import com.sokol.webgame.webgame.Repository.SetRepo;
 import com.sokol.webgame.webgame.entity.Set;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @AllArgsConstructor
 @Service
-public class SetService {
+public class GameService {
     private final SetRepo setRepo;
 
-    public List<Set> findAll() {
+    public List<Set> findAllSets() {
         return setRepo.findAll();
+    }
+
+    public byte[] getImages(String path) throws IOException {
+        InputStream imageStream = new ClassPathResource(path).getInputStream();
+        return imageStream.readAllBytes();
     }
 }
